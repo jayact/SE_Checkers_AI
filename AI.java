cce25bd4c8ac56d0d8592f29c9ffd2e43b7d8309
jayact <jayact13@gmail.com>
2013-11-05 17:58:47 -0500

    more classes, cleaned up some issues with board

---
+++ AI.java
@@ -0,0 +1,23 @@
+package checkers;
+
+public class AI  extends Player{
+	private Algorithm algo;
+	
+	public AI(char piece, Algorithm algo)
+	{
+		super(piece);
+		setAlgo(algo);
+	}
+	
+	public void setAlgo(Algorithm algo)
+	{
+		this.algo = algo;
+	}
+	
+	@Override
+	public Board takeTurn(Board b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ User.java
@@ -0,0 +1,20 @@
+package checkers;
+
+public class User extends Player 
+{
+	
+	public User(char piece)
+	{
+		super(piece);
+	}
+	
+	@Override
+	public Board takeTurn(Board b) {
+		// TODO Auto-generated method stub
+		//Call function to display board state, await user input
+		//Receive user input, make move
+		//return new board
+		return null;
+	}
+
+}
