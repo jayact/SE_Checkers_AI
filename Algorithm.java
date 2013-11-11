8670b2cfd85e408586145da8aed0733ed03733d6
jakraabit1 <jakraabit1@gmail.com>
2013-11-11 16:43:30 -0500

    added heuristic function
    rateBoard is now returning a value based on the number of pieces on each
    side of the board.

--- Algorithm.java
+++ Algorithm.java
@@ -1,6 +1,31 @@
-package checkers;
 
-public interface Algorithm 
+
+public abstract class Algorithm 
 {
-
+	/**
+	 * Rates a board by a heuristic value.
+	 * @param b
+	 * @return
+	 */
+	protected int rateBoard(Board b, char piece)
+	{
+            int hvalue = 0;
+		if(piece=='R')
+                {
+                    hvalue += (b.rUnits()-b.bUnits());
+                }
+                else
+                {
+                    hvalue += (b.bUnits()-b.rUnits());
+                }
+             return hvalue;
+	}
+	
+	/**
+	 * Uses the algorithm specific to each individual class to choose a board
+	 * @param b is the board
+	 * @param piece is the piece
+	 * @return the winning board.
+	 */
+	public abstract Board getMove(Board b, char piece);
 }
