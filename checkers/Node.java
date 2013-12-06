0757a2256c261eeaa4145f6be085458be45f38bf
jakraabit1 <jakraabit1@gmail.com>
2013-12-06 17:53:50 -0500

    altered node class to have parent stored as Node

--- checkers/Node.java
+++ checkers/Node.java
@@ -1,4 +1,4 @@
-package checkers.checkers;
+package checkers;
 
 
 import java.util.LinkedList;
@@ -14,9 +14,10 @@ import java.util.LinkedList;
  * @author Jack
  */
 public class Node {
-    Board parent;
+    Node parent;
     Board current;
     LinkedList<Node> children = new LinkedList<>();
+    int rating;
     private static int boardSize = 8;
     
     /**
@@ -24,7 +25,7 @@ public class Node {
      * @param parent the board state previous to the last move
      * @param current the current board state
      */
-    public Node(Board parent, Board current)
+    public Node(Node parent, Board current)
     {
         this.parent = parent;
         this.current = current;
@@ -40,33 +41,97 @@ public class Node {
     {
         for(Board b: current.finalMoves(piece))
         {
-            children.add(new Node(current, b));
+            Node temp = new Node(this, b);
+            //temp.rateBoard(b, piece);
+            children.add(temp);
         }
-//        Board temp = current;
-//        for(int i=0;i<boardSize;i++)
-//        {
-//            for(int j=0;j<boardSize;j++)
-//            {
-//                Piece tempP = temp.getPiece(i,j);
-//                if(tempP.getColor() == piece)
-//                {
-//                    for(Piece p: temp.finalMoves(tempP))
-//                    {
-//                        temp.makeMove(tempP, p);
-//                        children.add(new Node(current, temp));
-//                        temp = current;
-//                    }
-//                }
-//            }
-//        }
     }
+    
+    /**
+         * Overall heuristic function of the board
+         * @param b rated board
+         * @param piece player to give value to
+         * @return final heuristic value
+         */
+        public void rateBoard(char piece)
+        {
+            int scalar1 = 1;
+            int scalar2 = 1;
+            rating = (scalar1*h1(current, piece) + (scalar2*h2(current, piece)));
+        }
+        
+        public void setRating(int rate)
+        {
+            rating = rate;
+        }
+        
+	/**
+	 * Rates a board by the number of pieces.
+	 * @param b
+	 * @return
+	 */
+	private int h1(Board b, char piece)
+	{
+            int result = 0;
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    Piece p = b.getPiece(i, j);
+                    int pVal = 0;
+                    if(p.getColor() == piece)
+                        pVal++;
+                    else if(p.getColor() != '-')
+                        pVal--;
+                    result += pVal;
+                }
+            }
+            return result;
+	}
+        
+        /**
+         * Rates a board by the position advantage
+         * @param b
+         * @param piece
+         * @return 
+         */
+        private int h2(Board b, char piece)
+        {
+            int kingVal = 2;
+            int result = 0;
+            int[][] posVals = {{2,2,2,2,2,2,2,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,1,1,1,1,1,1,2},
+                               {2,2,2,2,2,2,2,2}};
+            
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    Piece p = b.getPiece(i, j);
+                    int pVal = 0;
+                    if(p.getColor() == piece)
+                        pVal = posVals[i][j];
+                    else if(p.getColor() != '-')
+                        pVal = -posVals[i][j];
+                    if(p.isKing())
+                        pVal = pVal*kingVal;
+                    result += pVal;
+                }
+            }
+            return result;
+        }
     
     
     /**
      * Accessor for parent
      * @return 
      */
-    public Board getParent()
+    public Node getParent()
     {
         return parent;
     }
@@ -87,6 +152,15 @@ public class Node {
     public LinkedList<Node> getChildren()
     {
         return children;
+    }
+    
+    /**
+     * Accessor for current rating
+     * @return 
+     */
+    public int getRating()
+    {
+        return rating;
     }
     
 }
